ea2dc3c18c216a2917d8a25428e3298ed155185c
Alberto Irurueta <alberto@irurueta.com>
2021-06-13 23:53:27 +0200

    Code quality improvements

--- src/test/java/com/irurueta/navigation/inertial/calibration/magnetometer/PROSACRobustKnownPositionAndInstantMagnetometerCalibratorTest.java
+++ src/test/java/com/irurueta/navigation/inertial/calibration/magnetometer/PROSACRobustKnownPositionAndInstantMagnetometerCalibratorTest.java
@@ -16723,7 +16723,7 @@ public class PROSACRobustKnownPositionAndInstantMagnetometerCalibratorTest imple
 
             assertNotNull(calibrator.getEstimatedCovariance());
             checkGeneralCovariance(calibrator.getEstimatedCovariance());
-            assertTrue(calibrator.getEstimatedMse() > 0.0);
+            assertTrue(calibrator.getEstimatedMse() >= 0.0);
             assertNotEquals(calibrator.getEstimatedChiSq(), 0.0);
 
             numValid++;
