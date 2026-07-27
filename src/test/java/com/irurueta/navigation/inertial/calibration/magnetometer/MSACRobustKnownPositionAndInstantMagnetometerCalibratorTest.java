27e6bab3c014d76f579e6ca5a024bbb6750b389c
Alberto Irurueta Carro <albertoicar@ext.inditex.com>
2026-07-27 15:37:23 +0200

    Test improvement

--- src/test/java/com/irurueta/navigation/inertial/calibration/magnetometer/MSACRobustKnownPositionAndInstantMagnetometerCalibratorTest.java
+++ src/test/java/com/irurueta/navigation/inertial/calibration/magnetometer/MSACRobustKnownPositionAndInstantMagnetometerCalibratorTest.java
@@ -8097,7 +8097,7 @@ class MSACRobustKnownPositionAndInstantMagnetometerCalibratorTest implements
             assertEstimatedResult(estimatedHardIron, estimatedMm, calibrator, true);
             checkGeneralCovariance(calibrator.getEstimatedCovariance());
             assertTrue(calibrator.getEstimatedMse() > 0.0);
-            assertTrue(calibrator.getEstimatedChiSq() > 0.0);
+            assertTrue(calibrator.getEstimatedChiSq() >= 0.0);
             assertTrue(calibrator.getEstimatedChiSqDegreesOfFreedom() > 0);
             assertTrue(calibrator.getEstimatedReducedChiSq() > 0.0);
             assertTrue(calibrator.getEstimatedP() >= 0.0);
