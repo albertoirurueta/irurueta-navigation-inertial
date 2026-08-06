c8171396e148436592c970e1b428fa9bed19e52a
Alberto Irurueta Carro <albertoirurueta@gmail.com>
2026-08-06 20:16:34 +0200

    Test improvements

--- src/test/java/com/irurueta/navigation/inertial/calibration/magnetometer/LMedSRobustKnownHardIronMagneticFluxDensityNormMagnetometerCalibratorTest.java
+++ src/test/java/com/irurueta/navigation/inertial/calibration/magnetometer/LMedSRobustKnownHardIronMagneticFluxDensityNormMagnetometerCalibratorTest.java
@@ -7032,7 +7032,7 @@ class LMedSRobustKnownHardIronMagneticFluxDensityNormMagnetometerCalibratorTest 
             assertNotNull(calibrator.getEstimatedCovariance());
             checkGeneralCovariance(calibrator.getEstimatedCovariance());
             assertTrue(calibrator.getEstimatedMse() >= 0.0);
-            assertTrue(calibrator.getEstimatedChiSq() > 0.0);
+            assertTrue(calibrator.getEstimatedChiSq() >= 0.0);
             assertTrue(calibrator.getEstimatedChiSqDegreesOfFreedom() > 0);
             assertTrue(calibrator.getEstimatedReducedChiSq() > 0.0);
             assertTrue(calibrator.getEstimatedP() >= 0.0);
